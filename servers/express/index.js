3f38b1b2be00564dc0af068c7815503d8aba5e33
nathanlao <gla93@sfu.ca>
2023-02-25 21:53:10 -0800

    logs incoming req and using template engine

--- servers/express/index.js
+++ servers/express/index.js
@@ -1,9 +1,10 @@
 // packages:
+var path = require('path')
+var serveIndex = require('serve-index')
+
 // Create a new express app
 var express = require('express')
 var app = express()
-
-var serveIndex = require('serve-index')
 
 // "process.env" is an object that contains all the Environment variables: 
 // specify the port
@@ -22,6 +23,24 @@ var options = {
 app.use('/', express.static('./pub_html', options))
 // 2. Serves a directory listing of "./pub_html/static-files" directory for the path "/static-files"
 app.use('/static-files', serveIndex('./pub_html/static-files', {'icons': true}))
+// 3.  Logs incoming requests to the console
+app.use('/', function(req, res, next) {
+    console.log(req.method, 'request for:', req.url, JSON.stringify(req.body))
+    next() // pass control to the next middleware function
+})
+
+// Parse incoming request bodies in JSON and URL-encoded data 
+// into JS object
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// 4. Set up EJS: template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/hello2', (req, res) => {
+    res.render('hello', {result: 'Hello World from EJS Template Engine!'})
+})
 
 // Port we are listening on
 app.listen(port, function() {
